2a00608a0e60211bb8b2ada2913ebb1de947c4e7
Rubens <paulo-rolim10@hotmail.com>
2024-09-19 08:13:51 -0300

    Criando a interface MedicoRepository

--- src/main/java/med/voll/api/repository/MedicoRepository.java
+++ src/main/java/med/voll/api/repository/MedicoRepository.java
@@ -1,8 +1,13 @@
 package med.voll.api.repository;
 
+import med.voll.api.endereco.Endereco;
 import med.voll.api.medico.Medico;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface Repository extends JpaRepository<Medico, Long> {
+import java.util.Optional;
 
+public interface MedicoRepository extends JpaRepository<Medico, Long> {
+@Query("SELECT e FROM Medico m JOIN m.endereco e WHERE e.logradouro = :logradouro AND e.bairro = :bairro AND e.cep = :cep AND e.cidade = :cidade AND e.uf = :uf AND e.numero != :numero")
+    Optional<Endereco> buscarEndereco(String logradouro, String bairro, String cep, String cidade, String uf, String numero);
 }

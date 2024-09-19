1a82b8bc8078445f18c510ae0c44e8342127fd41
Rubens <paulo-rolim10@hotmail.com>
2024-09-19 08:16:40 -0300

    Validação com Bean Validation

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,10 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.servico.Servico;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("medicos")
 public class MedicoController {
-
+    @Autowired
+    private Servico servico;
     @PostMapping
-    public void cadastrarMedicos(@RequestBody DadosCadastroMedico dados) {
-        System.out.println(dados);
+    @Transactional
+    public void cadastrarMedicos(@RequestBody @Valid DadosCadastroMedico dados) {
+        servico.addDadosMedico(dados);
     }
 }
--- src/main/java/med/voll/api/servico/Servico.java
+++ src/main/java/med/voll/api/servico/Servico.java
@@ -1,22 +1,32 @@
 package med.voll.api.servico;
 
+import med.voll.api.endereco.Endereco;
 import med.voll.api.medico.DadosCadastroMedico;
-import med.voll.api.model.Endereco;
-import med.voll.api.model.Medico;
-import med.voll.api.repository.Repository;
+import med.voll.api.medico.Medico;
+import med.voll.api.repository.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public class Service {
+import java.util.Optional;
+
+@Service
+public class Servico {
     @Autowired
-    private Repository repository;
+    private MedicoRepository repository;
 
     public void addDadosMedico(DadosCadastroMedico dados) {
         repository.save(converteMedicoDadosMedico(dados));
     }
 
     private Medico converteMedicoDadosMedico(DadosCadastroMedico dados) {
-        Medico medico = new Medico(dados.nome(), dados.email(), dados.crm(), dados.especialidade(),
-                new Endereco(dados.endereco().logradouro(), dados.endereco().bairro(), dados.endereco().cep(), dados.endereco().cidade(), dados.endereco().uf(), dados.endereco().numero(), dados.endereco().complemento()));
+        Medico medico = new Medico(dados.nome(), dados.email(), dados.telefone(), dados.crm(), dados.especialidade(),
+                verificarEndereco(new Endereco(dados.endereco().logradouro(), dados.endereco().bairro(), dados.endereco().cep(), dados.endereco().cidade(), dados.endereco().uf(), dados.endereco().numero(), dados.endereco().complemento())));
         return medico;
+    }
+
+    private Endereco verificarEndereco(Endereco endereco) {
+        Optional<Endereco> enderecoExistente = repository.buscarEndereco(endereco.getLogradouro(), endereco.getBairro(), endereco.getCep(), endereco.getCidade(), endereco.getUf(), endereco.getNumero());
+
+        return enderecoExistente.orElse(endereco);
     }
 }
